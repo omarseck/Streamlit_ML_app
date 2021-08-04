1e5d48b9c1e8c2fb66d42b72d171d2b43dea5202
omarseck <>
2021-08-04 11:15:28 +0200

    Modiffication de lappli

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
+import io
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.ensemble import RandomForestClassifier
@@ -15,74 +16,76 @@ from sklearn.model_selection import train_test_split
 plt.style.use('ggplot')
 
 
-df = pd.read_csv('income_prediction_dataset.csv')
-df.drop(['native-country', 'workclass', 'education', 'occupation',
-         'race', 'capital-gain', 'capital-loss'], axis=1, inplace=True)
-cat_col = list(df.select_dtypes(include='object').columns)
-num_col =[el for el in df.columns if el not in cat_col]
-y = df['gender']
-cat_col.remove('gender')
+file = st.file_uploader('AJouter un fichier', type = 'CSV')
+if file :
+  df = pd.read_csv(file)
+  df.drop(['native-country', 'workclass', 'education', 'occupation',
+          'race', 'capital-gain', 'capital-loss'], axis=1, inplace=True)
+  cat_col = list(df.select_dtypes(include='object').columns)
+  num_col =[el for el in df.columns if el not in cat_col]
+  y = df['gender']
+  cat_col.remove('gender')
 
-# Pre-process
+  # Pre-process
 
-df_cat = pd.get_dummies(df[cat_col])
-sc = StandardScaler()
-X_num = sc.fit_transform(df[num_col])
-df_num = pd.DataFrame(X_num, columns=num_col)
-X = pd.concat([df_num, df_cat], axis = 1)
-y = LabelEncoder().fit_transform(y)
-X_train,X_test, y_train, y_test = train_test_split(X, y,
-                                                   test_size = 0.33, random_state = 42)
-rfc = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
-                       criterion='gini', max_depth=None, max_features='sqrt',
-                       max_leaf_nodes=None, max_samples=None,
-                       min_impurity_decrease=0.0, min_impurity_split=None,
-                       min_samples_leaf=1, min_samples_split=2,
-                       min_weight_fraction_leaf=0.0, n_estimators=200,
-                       n_jobs=-1, oob_score=True, random_state=None, verbose=0,
-                       warm_start=False)
-rfc.fit(X_train, y_train)
-y_pred = rfc.predict(X_test)
+  df_cat = pd.get_dummies(df[cat_col])
+  sc = StandardScaler()
+  X_num = sc.fit_transform(df[num_col])
+  df_num = pd.DataFrame(X_num, columns=num_col)
+  X = pd.concat([df_num, df_cat], axis = 1)
+  y = LabelEncoder().fit_transform(y)
+  X_train,X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 0.33, random_state = 42)
+  rfc = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
+                        criterion='gini', max_depth=None, max_features='sqrt',
+                        max_leaf_nodes=None, max_samples=None,
+                        min_impurity_decrease=0.0, min_impurity_split=None,
+                        min_samples_leaf=1, min_samples_split=2,
+                        min_weight_fraction_leaf=0.0, n_estimators=200,
+                        n_jobs=-1, oob_score=True, random_state=None, verbose=0,
+                        warm_start=False)
+  rfc.fit(X_train, y_train)
+  y_pred = rfc.predict(X_test)
 
 
 
-st.title("Omar's first app")
+  st.title("Omar's first app")
 
-st.header('Data exploration')
+  st.header('Data exploration')
 
-option = st.sidebar.selectbox(
-    'Quelle colonne voulez vous afficher?',
-     cat_col)
+  option = st.sidebar.selectbox(
+      'Quelle colonne voulez vous afficher?',
+      cat_col)
 
-'Colonne: ', option
-f, axs = plt.subplots(figsize = (5,3))
-axs = sns.countplot(df[option])
-plt.xticks(rotation = 45)
-plt.title('Distribution de {}'.format(option))
-plt.show()
-st.pyplot(f)
+  'Colonne: ', option
+  f, axs = plt.subplots(figsize = (5,3))
+  axs = sns.countplot(df[option])
+  plt.xticks(rotation = 45)
+  plt.title('Distribution de {}'.format(option))
+  plt.show()
+  st.pyplot(f)
 
-option2 = st.sidebar.selectbox(
-    'Quelle colonne voulez vous afficher?',
-     num_col)
+  option2 = st.sidebar.selectbox(
+      'Quelle colonne voulez vous afficher?',
+      num_col)
 
-'Colonne: ', option2
+  'Colonne: ', option2
 
-f, axs = plt.subplots(figsize = (5,3))
-axs = sns.distplot(df[option2])
-plt.title('Distribution de {}'.format(option))
-plt.show()
-st.pyplot(f)
+  f, axs = plt.subplots(figsize = (5,3))
+  axs = sns.distplot(df[option2])
+  plt.title('Distribution de {}'.format(option))
+  plt.show()
+  st.pyplot(f)
 
-st.header('Modélisation')
-st.write(print(confusion_matrix(y_test, y_pred)))
-st.write(classification_report(y_test, y_pred))
+  st.header('Modélisation')
+  st.write(print(confusion_matrix(y_test, y_pred)))
+  st.write(classification_report(y_test, y_pred))
 
-f,axs = plt.subplots(figsize =(14,10))
-axs = sns.barplot(x = rfc.feature_importances_, y = X.columns)
-plt.title('Feature importantce')
-plt.show()
-st.pyplot(f)
+  f,axs = plt.subplots(figsize =(14,10))
+  axs = sns.barplot(x = rfc.feature_importances_, y = X.columns)
+  plt.title('Feature importantce')
+  plt.show()
+  st.pyplot(f)
 
 #'Starting a long computation...'
 
